6b4a64ba8eea8a0be7c4ed891816d843a1e635b4
vonirise <masumifelix7@gmail.com>
2025-06-25 11:32:17 +0800

    Tambah fungsi tambahItem() untuk menambahkan produk

--- Tokorootie.cpp
+++ Tokorootie.cpp
@@ -10,3 +10,28 @@ struct BakeryItem {
 };
 
 BakeryItem* head = nullptr;
+
+void tambahItem() {
+    string nama;
+    int harga, stok;
+    cout << "Masukkan nama produk    : ";
+    getline(cin, nama);
+    cout << "Masukkan harga produk   : ";
+    cin >> harga;
+    cout << "Masukkan stok produk    : ";
+    cin >> stok;
+    cin.ignore();
+
+    BakeryItem* produkBaru = new BakeryItem{nama, harga, stok, nullptr};
+
+    if (!head) {
+        head = produkBaru;
+    } else {
+        BakeryItem* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = produkBaru;
+    }
+    cout << "[✓] Produk berhasil ditambahkan.\n";
+}
